9e17420ab5c0147940f34f7a8a69020949efd71b
Shivang Parashar <shivangparashar66@gmail.com>
2025-09-14 16:04:10 +0530

    Add files via upload

---
+++ ques12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main() {
+    float a ;
+    
+    printf("enter the number:");
+    scanf("%f",&a);
+    if (a>0){
+    printf("the number is positive");}
+
+    else if (a<0){
+        printf("the number is negative");}
+
+    
+    else{
+        printf("the number is neither positive nor negative");
+    }
+    return 0;
+}
+
+    
+
+    
+    
+
+
+
+
+
+
+  
